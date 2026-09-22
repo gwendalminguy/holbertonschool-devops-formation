4c711978843d0643f675a47cc028694c13057a35
Gwendal <gwendalminguy@outlook.fr>
2026-09-22 15:48:43 +0200

    feat: add deploy() function

--- devops_culture_git/devops_bot.py
+++ devops_culture_git/devops_bot.py
@@ -8,5 +8,10 @@ def bot_status(name, energy):
     return f"{name} is online with {energy}% energy."
 
 
+def deploy():
+    return "Deployment started..."
+
+
 if __name__ == "__main__":
     print(bot_status("HolbieBot", 100))
+    print(deploy())
